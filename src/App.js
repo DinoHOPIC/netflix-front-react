7ce5816cd81b84b4b27d548c093813b7dc9dbcfb
Dino <dino.hopic@gmail.com>
2022-04-27 14:10:04 +0200

    second commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./assets/data.json";
+// console.log(data);
+// import logo from "./assets/images/logo.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  {
+    data.map(data.category);
+  }
+  return;
 }
 
 export default App;
