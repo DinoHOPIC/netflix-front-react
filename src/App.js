1127d16e08ae8fd3f66800a4d6caafc216a3e205
Dino <dino.hopic@gmail.com>
2022-04-27 15:02:04 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import "./App.css";
 import data from "./assets/data.json";
 
 function App() {
-  return <h1>{data.map(data.category)}</h1>;
+  return (
+    <div>
+      {data.map((categoryOfmovies, index) => {
+        return (
+          <div key={index}>
+            <h2>{categoryOfmovies.category}</h2>
+            {categoryOfmovies.images.map((imagesOfmovies, index) => {
+              // console.log(imagesOfmovies);
+              return <img key={index} src={imagesOfmovies} alt="" />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
